1c8a7e7d1dbe1c0fc44da39124a7122a79840293
Alex <alexis.amand@gmail.com>
2018-10-03 19:28:25 +0200

    Essai d'intégration d'un champ de recherche

---
+++ test.php
@@ -0,0 +1,120 @@
+<!doctype html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8">
+            <title>Histoires de Poilus</title>
+
+            <meta content="Biographies de poilus qui ont participé à la Première Guerre Mondiale de 1914 à 1918. Divers relevés de morts pour la France. Présentation de divers camps de prisonniers allemands." name="description">
+            <meta name="google-site-verification" content="NrJvg2SL3r8GToGISpF-SJatGnKIvS5mekxb-2uTef4" />
+
+            <link rel="stylesheet" href="css/style.css">    
+            <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+            <link rel="icon" type="image/png" href="/images/favicon.png" />
+    
+      </head>
+      <body>
+
+        <!-- code js pour l'avertissement sur les cookies -->
+
+        <script src="js/cookiechoices.js"></script>
+        <script>
+          document.addEventListener('DOMContentLoaded', function(event) {
+            cookieChoices.showCookieConsentBar('En naviguant sur le site, vous acceptez l’utilisation de cookies pour vous proposer une navigation optimale et me permettre de réaliser des statistiques de visites.',
+              'Ok', 'En savoir plus', 'cookies.php');
+          });
+        </script>
+
+        <header>
+	
+            <div id="rezo">
+            	<form action="machin.php" methode="POST">
+            	<input type="text" style="border:0px solid black;">
+            	<button type="submit" value="Ok" style="border:1px solid red;">
+            	</form>
+            	<br />
+            
+            	<a href="https://www.facebook.com/genealexis.fr" title="voir la page Facebook du site">
+            		<img src="images/facebook.png" /></a>
+            		<img src="images/googleplus.png" />
+           	 	<a href="https://twitter.com/alexisamand?lang=fr" title="me suivre sur Twitter">
+            		<img src="images/twitter.png" /></a>
+            </div>
+      
+            <a href="index.php" title="retour sur la page d'accueil du site">
+            <img src="images/logo.png" class="logo" alt="Histoires de Poilus"/></a>
+
+        </header>
+        
+        <nav>
+	      <a href="contact.php">Contact</a>
+            <a href="webographie.php">Webographie</a>
+            <a href="lieux.php">Les Batailles</a>
+            <a href="camps.php">Les Camps</a>
+            <a href="releves.php">Les Relevés</a>
+            <a href="hopitaux/hopitaux.php">Les Hôpitaux</a>
+            <a href="poilus.php">Les Poilus</a>     
+        </nav>
+
+        <section>
+
+	        <article style="margin-left:0px;">
+
+            <h1>Edito</h1>
+
+            <div id="colindex">
+    
+            <p>A l’origine, je devais continuer ma série d’articles sur le thème de la Première Guerre Mondiale en vous parlant de mes poilus sur mon site <a href="http://www.genealexis.fr">Genealexis</a>. Mouais… dit comme ça, je m'aperçois que c’est loin d’être génial.</p>
+
+                <figure>
+                <img src="images/image-diapo-1.jpg" alt="Affiche pour la mobilisation générale" />
+                </figure>
+            
+            <p>J’aurais certainement pu trouver une autre façon d’introduire la chose ? Peut-être que la solution était de vous raconter comment certains hommes de ma famille sont partis la fleur au fusil pour une guerre qu’ils pensaient courte ? Je suis désolé, mais je ne veux pas rentrer dans ce jeu-là.</p>
+    
+                <figure>
+                <img src="images/image-diapo-2.jpg" alt="Fiche de mort pour la France issue du site Mémoire des Hommes" />
+                </figure>
+           
+            <p>Aucun spécialiste de ce conflit, n’arrivera à me faire croire que des hommes dans la fleur de l’âge ont quitté leurs maisons, leurs travails, leurs femmes,  leurs enfants, leurs amis “avec plaisir”, je dirais plutôt qu’ils s’y étaient résignés. En 1994, Jean-Baptiste DUROSELLE (historien français) écrit: <i>« Qui va faire la moisson imminente ? Et comment, avec la réquisition de nombreux chevaux, pourra-t-on labourer en octobre, si toutefois, par impossible, la guerre n’est pas encore gagnée et terminée ? »</i>. Il y a bien eu des scènes festives, avec des wagons et des fusils fleuris, mais c’est surtout dû au fait que le  départ des troupes était fortement ritualisé: discours d'officiers, défilés dans la ville des casernes aux gares. Il ne faut pas oublier non plus que  la <strong>loi sur l'état de siège du 3 avril 1878</strong> attribue aux autorités militaires un large pouvoir, en particulier celui de sanctionner toute parole opposée à la guerre. On peut donc facilement comprendre que les mobilisés et les civils n’avaient pas vraiment le choix, mieux valait dire que l’on était “pour la guerre”. Si vous n’êtes pas convaincu, je vous invite à lire le mémoire d’Yves POURCHER intitulé <a href="http://terrain.revues.org/1027#txt">Les clichés de la Grande Guerre</a>.</p>
+
+                <figure>
+                <img src="images/image-diapo-3.jpg" alt="Carte postale représentant le départ des soldats pour le Front" />
+                </figure>
+
+            <p>Je vous propose donc, à travers ce site, un résumé des découvertes que j’ai eu la chance de réaliser à propos des membres de ma famille qui ont participé à la Première Guerre Mondiale. Vous allez ainsi découvrir que <a href="camps.php" title="Les camps de prisonniers pendant la Grande Guerre">certains ont été fait prisonniers</a> et emmenés en Allemagne, que certains ont été blessés en défendant leur patrie et enfin que <a href="releves.php">d’autres sont morts pour leur patrie</a>. Ce site est avant toute chose là pour rendre hommage à tous les soldats de la Der des Der, qu'ils soient <em>Poilus</em> en France, <em>Landsers</em> en Allemagne, <em>Tommies</em> en Angleterre, <em>Diggers</em> en Australie, <a href="poilus/soldats-belgique.php">Jass en Belgique</a>, <em>Sammies</em> aux États-Unis ou encore <em>Mehmetçik</em> en Turquie (Empire Ottoman).</p>
+
+            <p class="right">Bonne lecture,<br/>Alexis, webmaster.</p>
+            
+            </div>  
+
+            <p class="sources">
+            <?php setlocale(LC_ALL,'french');echo "Dernière modification effectuée le ".date("j/m/Y", getlastmod()); ?> 
+            </p>
+
+            </article>
+
+        </section>
+
+        <footer>
+            <a href="mentions.php">Mentions Légales</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="contact.php">Contact</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="http://www.genealexis.fr">Généalexis</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="cookies.php">Cookies</a>
+            <br /><br />Copyright &copy; 2014-<?php echo date('Y'); ?> - Alexis AMAND<br />Tous droits réservés
+        </footer>
+
+<?php include('include/stats.inc'); ?>
+
+<!-- javascript -->
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>    
+<script type="text/javascript" src="js/jquery.fancybox-1.3.4.pack.js"></script>
+
+<script type="text/javascript">  
+  $(document).ready(function() {
+	$("a#single_image").fancybox();
+  }); 
+</script>  
+
+</body>
+</html>
