d41d0ea45aefcebd672ab39f50a81fe7ae81a1a1
Alex <alexis.amand@gmail.com>
2019-12-10 14:29:41 +0100

    Ajout des quelques photos de tombes de poilus

--- releves/valenciennes.php
+++ releves/valenciennes.php
@@ -129,6 +129,20 @@
         <figcaption>Tombe d'André BERNARD<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
     </figure>
 
+    <h2>Lucien BODDAERT</h2>
+
+    <figure>
+        <a href="images/lucien-boddaert-01.jpg" data-fancybox="gallery" data-caption="Tombe de Lucien BODDAERT, mort pour la France en 1917">
+        <img src="images/mini/lucien-boddaert-01.jpg" alt="Tombe de Lucien BODDAERT, un poilu mort pour la France en 1917"/></a>
+        <figcaption>Tombe de Lucien BODDAERT<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
+    </figure>
+
+    <figure>
+        <a href="images/lucien-boddaert-02.jpg" data-fancybox="gallery" data-caption="Tombe de Lucien BODDAERT, mort pour la France en 1917">
+        <img src="images/mini/lucien-boddaert-02.jpg" alt="Tombe de Lucien BODDAERT, un poilu mort pour la France en 1917"/></a>
+        <figcaption>Tombe de Lucien BODDAERT<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
+    </figure>
+
     <h2>René et Maurice GOUVION</h2>
 
     <figure>
@@ -141,6 +155,18 @@
         <a href="images/gouvion-01.jpg" data-fancybox="gallery" data-caption="Tombe de René et Maurice GOUVION, morts pour la France pendant la Grande Guerre">
         <img src="images/mini/gouvion-01.jpg" alt="Tombe de René et Maurice GOUVION, deux poilus morts pour la France pendant la Grande Guerre"/></a>
         <figcaption>Tombe de René et Maurice GOUVION<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
+    </figure>
+    
+    <figure>
+        <a href="images/gouvion-maurice.jpg" data-fancybox="gallery" data-caption="Maurice GOUVION, mort pour la France pendant la Grande Guerre">
+        <img src="images/mini/gouvion-maurice.jpg" alt="Tombe de Maurice GOUVION, deux poilus mort pour la France pendant la Grande Guerre"/></a>
+        <figcaption>Tombe de Maurice GOUVION<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
+    </figure>
+
+    <figure>
+        <a href="images/gouvion-rene.jpg" data-fancybox="gallery" data-caption="RenéGOUVION, mort pour la France pendant la Grande Guerre">
+        <img src="images/mini/gouvion-rene.jpg" alt="Tombe de René GOUVION, deux poilus mort pour la France pendant la Grande Guerre"/></a>
+        <figcaption>Tombe de René GOUVION<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
     </figure>
 
     <h2>Lucien MASCLET</h2>
@@ -155,6 +181,20 @@
         <a href="images/lucien-masclet-01.jpg" data-fancybox="gallery" data-caption="Tombe de Lucien MASCLET, mort pour la France pendant la Grande Guerre">
         <img src="images/mini/lucien-masclet-01.jpg" alt="Tombe de Lucien MASCLET, poilu mort pour la France pendant la Grande Guerre"/></a>
         <figcaption>Tombe de Georges NOLLIN<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
+    </figure>
+    
+    <h2>René MIRLAND</h2>
+
+    <figure>
+        <a href="images/rene-mirland-01.jpg" data-fancybox="gallery" data-caption="Tombe de René MIRLAND, mort pour la France en 1915">
+        <img src="images/mini/rene-mirland-01.jpg" alt="Tombe de René MIRLAND, un poilu mort pour la France en 1915"/></a>
+        <figcaption>Tombe de René MIRLAND<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
+    </figure>
+
+    <figure>
+        <a href="images/rene-mirland-02.jpg" data-fancybox="gallery" data-caption="Tombe de René MIRLAND, mort pour la France en 1915">
+        <img src="images/mini/rene-mirland-02.jpg" alt="Tombe de René MIRLAND, un poilu mort pour la France en 1915"/></a>
+        <figcaption>Tombe de René MIRLAND<br/>cimetière Saint-Roch (Valenciennes)</figcaption>
     </figure>
     
 	<h2>Georges NOLLIN</h2>
