a376f8e377b302d7e5bfc14acc9c13ede607c9a6
Alex <alexis.amand@gmail.com>
2020-07-16 11:36:23 +0200

    Tuto

--- tutos/parcours-soldat.php
+++ tutos/parcours-soldat.php
@@ -58,16 +58,80 @@
 
             <h1>Comment retrouver le parcours d’un soldat ?</h1>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			
-          	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          	
-          	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          	
-          	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          	
-          	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-             
+            <p>Ce petit guide a pour but de vous aider à trouver des informations sur un individu qui a participé à la Première Guerre Mondiale. Il ne s’agit d’une méthode, mais plutôt de diverses pistes que vous pouvez tester. Je ne pourrais pas être exhaustif car chaque cas est différent en fonction, par exemple, du corps d’armée ou de la classe de mobilisation. Pour ce tutoriel, je vais mettre de côté le cas où vous avez déjà des informations, si vous en avez, on va dire que ça vous fera un petit plus.</p>
+
+            <h2>1) Les fiches matricules</h2>
+
+            <p>La fiche matricule d’un individu est un résumé de son parcours militaire. La plupart du temps, elle est établie pendant la vingtième année d’un homme, on parle alors de <strong>classe</strong>, en fonction de son domicile, on parle de <strong>bureau de recrutement</strong>. Par exemple, mes ancêtres qui ont vécu dans le village de Sebourg (Nord) dépendent du bureau de recrutement de Valenciennes, alors que ceux qui ont vécu dans le petit village de Bétheniville (Marne) dépendent de celui de Reims. Les individus d’une même classe et d’un même bureau sont ensuite regroupés au sein de listes alphabétiques appelées, par exemple, tables alphabétiques sur le site des <a href="#">Archives Départementales du Nord</a>. Pour le Bureau de Dunkerque (Nord), la table alphabétique de 1884 ressemble à ça :</p>
+
+            <figure>
+                <img src="images/dunkerque-1884.jpg" style="height:60%;"> 
+                <figcaption>Extrait...</figcaption>
+            </figure>
+
+            <p>Chaque jeune homme se voit attribuer un nombre, que l’on appelle « matricule », c’est celui qui va vous aider à trouver sa fiche.  Si vous n’avez pas trouvé votre ancêtre dans la classe qui correspond à l’année de ses 20 ans, jetez un coup d’œil à l’année de ses 19 ou 21 ans, on ne sait jamais. Et surtout, j’insiste bien sur le fait qu’il ne faut pas  chercher en fonction du lieu de naissance, mais bien sur le domicile du conscrit. Tout va bien jusque là ? Super ! On continue.</p>
+
+            <p>Si on résume, dans l’état actuel de vos recherches, vous avez les éléments suivants :</p>
+
+            <ul>
+                <li>Un bureau, qui dépend du lieu où vivait le jeune homme</li>
+                <li>Une classe, la plupart du temps elle correspond à l’année de ses 20 ans</li>
+                <li>un numéro de matricule que vous avez trouvé sur les « Tables alphabétiques »</li>
+            </ul>
+
+            <h2>2) le cas des prisonniers de guerre</h2>
+
+            <p>Si, en parcourant la fiche matricule que vous venez de trouver, vous avez découvert que votre aïeul a été prisonnier en Allemagne (ou ailleurs…) pendant la Grande Guerre, vous avez une chance de trouver des informations complémentaires sur le site de la Croix Rouge :</p>
+
+            <a href="http://grandeguerre.icrc.org/fr">http://grandeguerre.icrc.org/fr</a>
+
+            <figure>
+                <img src="images/grandeguerre-01.jpg" style="width:60%;"> 
+                <figcaption>Extrait...</figcaption>
+            </figure>
+
+            <p>Je vous laisse faire une recherche via le formulaire proposé sur la page d’accueil du site. Si le conscrit pour lequel vous faites des recherches est présent dans la base de données du CICR, les résultats vont apparaître sous la forme de petites fiches classées par ordre alphabétique. N’hésitez pas à faire des essais avec des variantes du patronyme.</p>
+
+            <figure>
+                <img src="images/grandeguerre-01.jpg" style="width:60%;"> 
+                <figcaption>Extrait...</figcaption>
+            </figure>
+
+            <p>Chaque fiche contient un certain nombre de références (sur l’exemple ci-dessus R100124) qui renvoient à leur tour vers d’autre document que je vous laisse découvrir.</p>
+
+            <h2>3) le cas des morts pour la France</h2>
+
+            <h3>3.1) Mémoire des Hommes</h3>
+
+            <p>Mémoire des Hommes est un site qui dépend du Service Historique de la Défense, via le Ministère des Armées. Il s’agit d’une base de données qui regroupe une grande quantité de données sur les personnes qui ont servi au cours des différents conflits qui ont jalonné notre histoire. La base la plus utilisée par les généalogistes est sans aucune doute la base des « Morts pour la France de la Première Guerre mondiale » :</p>
+
+            https://www.memoiredeshommes.sga.defense.gouv.fr/fr/article.php?larub=24&titre=morts-pour-la-france-de-la-premiere-guerre-mondiale 
+
+            <figure>
+                <img src="images/mdh-shd.jpg" style="width:60%;"> 
+                <figcaption>Extrait...</figcaption>
+            </figure>
+
+            <p>Un clic sur le gros bouton bleu nommé « faire une recherche » vous donne accés un petit formulaire de recherche, pour ce petit tutoriel, j’ai déjà rempli le champ  « nom » avec le patronyme de l’un de mes arrière-arrière-grand-pères. A noter au passage que presque 1,5 millions de  soldats morts pour la France ont été indexés par des bénévoles via un mouvement initié par le site http://www.1jour1poilu.com</p> 
+
+            <figure>
+                <img src="images/mdh-shd-02.jpg" style="width:60%;"> 
+                <figcaption>Extrait...</figcaption>
+            </figure>
+
+            <p>Même si une recherche par nom peut être suffisante, je vous conseille de mettre le maximum d’informations pour limiter le nombre de résultats. Comme pour le site de la Croix Rouge dont nous avons parlé tout à l’heure, n’hésitez pas à faire d’autres essais avec des variantes du patronyme, en jouant par exemple sur le nombre de L ou le nombre de P. Si une piste de recherche est trouvée dans la base de données, les resultats de votre recherche apparaitront sous la forme d’une liste :</p>
+
+            <figure>
+                <img src="images/mdh-shd-02.jpg" style="width:60%;"> 
+                <figcaption>Extrait...</figcaption>
+            </figure>
+
+            <h3>3.2) Les JMO</h3>
+
+            <h3>3.2) Livres d’or</h3>
+
+            <h3>3.3) Mémorial GenWeb</h3>
+
         </article>
 
     </section>
